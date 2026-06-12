4fedbb89cfe2bfd3a836e178b29abed81c976508
o2yang <56992354+o2yang@users.noreply.github.com>
2026-06-12 23:01:23 +0800

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded_string;
+        if (strs.size() == 0) return "";
+        for (auto& s : strs) {
+            encoded_string.append(to_string(s.size()));
+            encoded_string.append(" ");
+            encoded_string.append(s);
+        }
+        cout << encoded_string << endl;
+        return encoded_string;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded_string;
+        int length;
+        if (s.size() == 0) return {};
+
+        int i = 0, j = i + 1;
+        while (i < s.size()) {
+            if (s[j] == ' ') {
+                length = length * 10 + int(s[i] - '0');
+                cout << length << endl;
+                cout << s.substr(j+1, length) << endl;
+                decoded_string.push_back(s.substr(j+1, length));
+                i = i + length + 2;
+                j = i + 1;
+                length = 0;
+            }
+            else {
+                length = length * 10 + int(s[i] - '0');
+                i++;
+                j++;
+            }
+        }
+        return decoded_string;
+    }
+};

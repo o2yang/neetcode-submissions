42b43bc1ee4e090f885548c521227b0d548265d2
o2yang <56992354+o2yang@users.noreply.github.com>
2026-06-17 00:05:35 +0800

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> output;
+        unordered_set<int> target_already_used;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            int target = -nums[i];
+            int begin_ptr = i + 1;
+            int end_ptr = nums.size()-1;
+            int sum;
+            if (target_already_used.count(target)) continue;
+            else target_already_used.insert(target);
+            while (begin_ptr < end_ptr) {
+                sum = nums[begin_ptr] + nums[end_ptr];
+                if (sum == target) {
+                    output.push_back({-target, nums[begin_ptr], nums[end_ptr]});
+                    end_ptr --;
+                    begin_ptr ++;
+                    while (begin_ptr < end_ptr && nums[begin_ptr] == nums[begin_ptr-1]) {
+                        begin_ptr ++;
+                    }
+                }
+                else if (sum > target) end_ptr --;
+                else begin_ptr ++;
+            }
+        }
+
+        return output;
+    }
+};

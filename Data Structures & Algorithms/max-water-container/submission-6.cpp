3a14366db3ed9855c0d7d66ebd72bfc230d72075
o2yang <56992354+o2yang@users.noreply.github.com>
2026-06-17 21:09:12 +0800

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int left_ptr = 0;
+        int right_ptr = heights.size() - 1;
+        int max_amount = -1;
+        
+        
+        while (left_ptr < right_ptr) {
+            int area = min(heights[left_ptr], heights[right_ptr]) * (right_ptr - left_ptr);
+            cout << area << " " << min(heights[left_ptr], heights[right_ptr]) << " " << heights[left_ptr] << " " << heights[right_ptr] << " " << endl;
+            if (area > max_amount) {
+                max_amount = area;
+            }
+            if (heights[left_ptr] < heights[right_ptr]) left_ptr ++;
+            else right_ptr --;
+        }
+        return max_amount;
+    }
+};

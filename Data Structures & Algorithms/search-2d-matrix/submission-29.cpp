c244df168d95ef199aa9fd99bdb0989d9dbb1071
o2yang <56992354+o2yang@users.noreply.github.com>
2026-06-20 02:06:21 +0800

    Add: search-2d-matrix - submission-29

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-29.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int begin_ptr = 0;
+        int end_ptr = matrix.size() - 1;
+        int middle_ptr;
+
+        // First round => find the target_row
+        while (begin_ptr <= end_ptr) {
+            middle_ptr = (begin_ptr + end_ptr) / 2;
+            cout << middle_ptr << " " << matrix[middle_ptr][0] << endl;
+            if (matrix[middle_ptr][0] == target) {
+                cout << "found" << endl;
+                return true;
+            }
+            else if (matrix[middle_ptr][0] < target) {
+                if (begin_ptr != end_ptr) begin_ptr = middle_ptr + 1;
+            }
+            else {
+                if (begin_ptr != end_ptr) end_ptr = middle_ptr - 1;
+            }
+            if ((end_ptr - begin_ptr) == 1 || end_ptr == begin_ptr) {
+                if ((matrix[begin_ptr][0] > target) && begin_ptr != 0) begin_ptr--;
+                break;
+            }
+        }
+        // Second round => find the target according to the target_row
+        int target_row = begin_ptr;
+        cout << "target_row: " << target_row << endl;
+        begin_ptr = 0;
+        end_ptr = matrix[0].size() - 1;
+        while (begin_ptr <= end_ptr) {
+            middle_ptr = (begin_ptr + end_ptr) / 2;
+            if (matrix[target_row][middle_ptr] == target) {
+                return true;
+            }
+            else if (matrix[target_row][middle_ptr] < target) {
+                begin_ptr = middle_ptr + 1;
+            }
+            else {
+                end_ptr = middle_ptr - 1;
+            }
+        }
+        return false;
+    }
+};
